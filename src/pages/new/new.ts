7f9c78a312ed4e04d9f7c347f82f96e237c07c16
Riyad Khalifeh <RiyadKhalifehIT@gmail.com>
2018-01-26 21:37:42 +0200

    Add files via upload

---
+++ src/pages/new/new.ts
@@ -0,0 +1,26 @@
+
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the NewPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-new',
+  templateUrl: 'new.html',
+})
+export class NewPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    //alert('ionViewDidLoad NewPage');
+  }
+
+}
